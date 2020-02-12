deb1dfebfbc3a29a4d5d707a7c85b79acbb6ba08
gyd1997 <gyd0008@163.com>
2020-02-12 13:04:45 +0800

    '详情页导航栏完成'

---
+++ src/views/detail/chidCpn/DetailNavbar.vue
@@ -0,0 +1,59 @@
+<template>
+  <navbar class="navbar">
+    <div slot="left" class="back" @click="backClick">
+      <img src="~assets/img/common/back.svg" alt="">
+    </div>
+    <div slot="center" class="title">
+      <div class="title-item"
+           v-for="(item, index) of titles"
+           :key="index"
+           @click="titleClick(index)"
+           :class="{active: currentIndex == index}">
+        {{item}}
+      </div>
+    </div>
+  </navbar>
+</template>
+
+<script>
+  import Navbar from 'components/common/navbar/Navbar.vue'
+  export default {
+    name: "DetailNavbar",
+    components: {
+      Navbar
+    },
+    data() {
+      return {
+        titles: ['商品', '参数', '评论', '推荐'],
+        currentIndex: 0
+      }
+    },
+    methods: {
+      titleClick(index) {
+        this.currentIndex = index
+      },
+      backClick() {
+        this.$router.back()
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  .navbar {
+    border-bottom: 1px solid #eee;
+  }
+  .title {
+    display: flex;
+    font-size: 13px;
+  }
+  .title-item {
+    flex: 1;
+  }
+  .active {
+    color: var(--color-high-text);
+  }
+  .back {
+    margin-top: 5px;
+  }
+</style>
