31e05b0962e0c5eb6abf8b807b239c8264d940bc
gyd1997 <gyd0008@163.com>
2020-02-13 17:39:57 +0800

    '商品数据展示'

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,20 +2,24 @@
   <div>
     <detail-navbar/>
     <detail-swiper :topImgs="topImgs"/>
+    <detail-base-info :goods="goods"/>
   </div>
 </template>
 
 <script>
   import DetailSwiper from './chidCpn/DetailSwiper'
   import DetailNavbar from './chidCpn/DetailNavbar'
-  import { getDetail } from "network/detail"
+  import DetailBaseInfo from './chidCpn/DetailBaseInfo'
+  import { getDetail, Goods, Shop } from "network/detail"
 
   export default {
     name: "Detail",
     data() {
       return {
         iid: null,
-        topImgs: []
+        topImgs: [],
+        goods: {},
+        shop: {}
       }
     },
     created() {
@@ -24,13 +28,17 @@
     },
     components: {
       DetailNavbar,
-      DetailSwiper
+      DetailSwiper,
+      DetailBaseInfo
     },
     methods: {
       // 网络请求
       getDetail(iid) {
         getDetail(iid).then(res => {
-          this.topImgs = res.result.itemInfo.topImages
+          const data = res.result
+          this.topImgs = data.itemInfo.topImages
+          this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+          this.shop = new Shop(data.shopInfo)
         })
       }
     }
